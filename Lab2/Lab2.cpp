ff35c51dd43a74749de1d2c4426a83027d12a403
Eugenia <grossuevgenia@gmail.com>
2020-11-04 00:47:51 +0700

    lab9

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include<malloc.h>
 #include<string.h>
-
+#pragma warning(disable:4996)
 #define LEN 100
 #define standart "no_name"
 using namespace std;
@@ -17,6 +17,31 @@ class pets
 public:
     pets() {
         name=standart;
+        age = 0;
+        wheit = 0;
+        price = 0;
+    }
+    pets(char* t, string name, int age, int wheit, double price)
+    {
+        type_pets = new char[20];
+        strcpy(type_pets, t);
+        this->name = name;
+        this->age = age;
+        this->wheit = wheit;
+        this->price = price;
+    }
+    pets(const pets& p)
+    {
+        this->type_pets=p.type_pets;
+        this->name = p.name;
+        this->age = p.age;
+        this->wheit = p.wheit;
+        this->price = p.price;
+
+    }
+    pets(string name)
+    {
+        this->name = name;
         age = 0;
         wheit = 0;
         price = 0;
@@ -80,14 +105,11 @@ public:
 
     void output()
     {
-        // cout << "The numbers of pets : " << col << endl;
-        // cout << "Store profit is " << income << endl;
-        // cout << "Store damages is " << damages << endl;
-        // cout << "Food price is " << price_feed << endl;
-        cout << "Pet's name " << name << endl;
-        cout << "Pet's age " << age << endl;
-        cout << "Pet's wheit " << wheit << endl;
-        cout << "Pet's price " << price << endl;
+        cout << "Pet's name- " << name << " age- " << age << " wheit- " << wheit << " price- " << price << endl;
+    }
+    void output_for_copy()
+    {
+        cout << "Pet's type"<<type_pets<<" name- " << name << " age- " << age << " wheit- " << wheit << " price- " << price << endl;
     }
     /* */
     double add_price(double sum)
@@ -98,6 +120,7 @@ public:
         return sum;
     }
 private:
+    char* type_pets;
     string name;
     int age;
     int wheit;
@@ -112,6 +135,38 @@ public:
         col = 0;
         income = 0;
         damages = 0;
+    }
+    zooshop(char *n, int col, double income, double damages, pets pet)
+    {
+        names = new char[10];
+        strcpy(this->names, n);
+        this->col = col;
+        this->income = income;
+        this->damages = damages;
+        for (int i = 0; i < col; i++)
+        {
+            this->pet[i] = pet;
+        }
+    }
+    zooshop &operator = (zooshop &s)
+    {
+        if (names)
+        {
+            delete names;    
+        }
+        names  = NULL;
+        this->col = s.col;
+        int l = strlen(s.names) + 1;
+        names = new char[l];
+        strcpy(this->names, s.names);
+        
+        this->income = s.income;
+        this->damages = s.damages;
+        for (int i = 0; i < col; i++)
+        {
+            this->pet[i] = s.pet[i];
+        }
+        return *this;
     }
     zooshop(int col, double income, double damages, pets pet[LEN])
     {
@@ -169,23 +224,14 @@ public:
 
     void read()
     {
-        cout << "The numbers of pets : " << col << endl;
-        cout << "Store profit is " << income << endl;
-        cout << "Store damages is " << damages << endl;
-        cout << "Food price is " << price_feed << endl;
+        cout << "The numbers of pets : " << col << " store profit is " << income << " store damages is " << damages<< " food price is " << price_feed << endl;
     }
     void output()
     {
-        cout << "The numbers of pets : " << col << endl;
-        cout << "Store profit is " << income << endl;
-        cout << "Store damages is " << damages << endl;
-        cout << "Food price is " << price_feed << endl;
+        cout << "The numbers of pets : " << col << " store profit is " << income << " store damages is " << damages << " food price is " << price_feed << endl;
         for (int i = 0; i < col; i++)
         {
-            cout << "Pet's name " << pet[i].getname() << endl;
-            cout << "Pet's age " << pet[i].getage() << endl;
-            cout << "Pet's wheit " << pet[i].get_wheit() << endl;
-            cout << "Pet's price " << pet[i].get_price() << endl;
+            cout << "Pet's name- " << pet[i].getname() << " age- " << pet[i].getage()<< " wheit- " << pet[i].get_wheit() << " price- " << pet[i].get_price() << endl;
             cout << endl;
         }
     }
@@ -226,7 +272,7 @@ public:
     }
     friend void check(zooshop sale);
     
-    friend zooshop operator+(zooshop s,zooshop s1);
+    /*friend zooshop operator+(zooshop s,zooshop s1);
     zooshop& operator++()
     {
         this->price_feed++;
@@ -238,7 +284,7 @@ public:
         ++*this;
         return s;
 
-    }
+    }*/
      void found_pet(string names)
     {
         int res = 0; 
@@ -270,7 +316,12 @@ public:
          else 
          { cout << "more animals in the second store" << endl; }
      }
+     void copy()
+     {
+         cout << "The store names"<<names<<" the numbers of pets : " << col << " store profit is " << income << " store damages is " << damages << " food price is " << price_feed << endl;
+     }
 private:
+    char* names;
     int col;
     double income;
     double damages;
@@ -290,11 +341,11 @@ void check(zooshop sale)
     }
     cout << "Checked ended" << endl;
 }
-zooshop operator+(zooshop s, zooshop s1)
+/*zooshop operator+(zooshop s, zooshop s1)
 {
   s1.set_col( s.col + s1.col);
   return s1;
-}
+}*/
 
 int main()
 {
@@ -305,11 +356,45 @@ int main()
     double profit = 0;
     string st = "no_name";
     cout << "Working with static object" << endl;
-    pets pet(st, 0, 0, 0);
+
+    pets pet(st, 0, 0, 0);//constructor call with all value
+    cout << "for class pet: with all value" << endl;
+    pet.output();
+    pets pet9("dogy");//constructor call with one value
+    cout << "for class pet: with one value" << endl;
+    pet9.output();
+    pets pet9_1;//constructor call without value
+    cout << "for class pet: without value" << endl;
+    pet9_1.output();
+    string cat = "cat";
+    string dog = "dog";
+    pets pet9_2[2] = {cat,dog};//constructor call with one value and array 
+    char* type;
+    type = new char[10];
+    strcpy(type, "cat");
+    //overload
+    pets pet93(type, "bar", 1, 1, 1);
+    pets pet933 = pet93;
+    cout << "overload" << endl;
+    pet933.output_for_copy();
+    
+
     pet.input();
     sum = pet.add_price(sum);
-    zooshop store(1, 0, 0, pet);
+    zooshop store(1, 0, 0, pet);//constructor with one value 
+    cout << "for class zooshop : wit all value (one value pet)" << endl;
     store.output();
+    cout << "for class zooshop : without value" << endl;
+    zooshop store9;//constructor without value
+    store9.output();
+    cout << "for class zooshop : with all value (array's pet)" << endl;
+    zooshop store91(1, 0, 0, pet9_2);//constructor with array
+    store91.output();
+    
+  
+   
+
+
     store.feeding();
     store.output();
     store.sale();
@@ -318,16 +403,28 @@ int main()
     store.this_profit(&profit);
 
     check(store);
-    zooshop store10(1, 0, 0,  pet);
+    /*zooshop store10(1, 0, 0,  pet);
     store10 = store + store10;
     store10.output();
     store10 = store++;
     store10.output();
     store10 = ++store;
-    store10.output();
+    store10.output();*/
 
     pets pet2[LEN];
-    cout << "\nWorking with an array of static object" << endl;
+    cout << "\nWorking with dinamic object" << endl;
+
+    //constructor for dynamic object
+    pets* pet99 = new pets();
+    pets* pet991 = new pets("cota");
+    pets* pet992 = new pets("cata", 1, 1, 5);
+    cout << "for class pet: with all value" << endl;
+    pet992->output();
+    cout << "for class pet: with one value" << endl;
+    pet991->output();
+    cout << "for class pet: without value" << endl;
+    pet99->output();
+    
     cout << "Enter nunber of pets in shoop" << endl;
     cin >> col;
     for (int i = 0; i < col; i++)
@@ -335,9 +432,24 @@ int main()
         pet2[i].input();
         sum = pet2[i].add_price(sum);
     }
-
+    //constructor call with array
     zooshop* store2 = new zooshop(col, 0, 0, pet2);
+    zooshop* store99 = new zooshop(1, 0, 0, pet992);
+    zooshop* store991 = new zooshop();
+    cout << "for class zooshop : wit all value (one value pet)" << endl;
+    store99->output();
+    cout << "for class zooshop : without value" << endl;
+    store991->output();
+    cout << "for class zooshop : with all value (array's pet)" << endl;
     store2->output();
+    char* namess;
+    namess = new char[10];
+    strcpy(namess, "names");
+    zooshop* store992 = new zooshop(namess, 1, 0, 0, pet);
+    zooshop* store993 = store992;
+    cout << "deep copy" << endl;
+    store993->copy();
+    cout << endl;
     store2->feeding();
     //for 8 lab
     pets pet8(st, 0, 0, 0);
@@ -347,10 +459,10 @@ int main()
     zooshop::compare(*store2, store8);
     zooshop::set_price_feed(15);
     cout << "New price feed "<< zooshop::get_price_feed()<< endl;
-    string names;
+    string names1;
     cout << "Enter pet's name" << endl;
-    cin >> names;
-    store2->found_pet(names);
+    cin >> names1;
+    store2->found_pet(names1);
     store2->output();
     store2->sale();
     store2->output();
